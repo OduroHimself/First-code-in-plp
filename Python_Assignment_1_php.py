5af17f1739fe7bae87349ab6c42d38472062c6ec
OduroHimself <oduroemmanuelofori@gmail.com>
2025-03-15 16:40:24 +0000

    Python Assinment 1

--- Python_Assignment_1_php.py
+++ Python_Assignment_1_php.py
@@ -1,15 +1,15 @@
-print("Hello, what do you want to do today")
+print("Hello, what do you want to do today") # Requests for the user's decision
 print("Press a for addition")
 print("Press s for subtraction")
 print("Press m for multiplication")
 print("Press d for division")
 
-x = input(str())
+x = input(str()) #Carries user's decision
 
 first_value = float(input("Please enter the first value for your operation:")) # Requests for user to input first value
 second_value = float(input("Please enter the second value for your operation:")) # Requests for user to input second value
 
-
+# Calculates and prints out the user's answer
 if x ==  "a":
     y = first_value + second_value
     print("Your answer is: ", y)
