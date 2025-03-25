1c555ed0cf12503ae0199713d0faea6aebbe449a
OduroHimself <oduroemmanuelofori@gmail.com>
2025-03-25 20:00:02 +0000

    Python Assignment 2

---
+++ Python_Assignment_2.py
@@ -0,0 +1,32 @@
+# creating empty list
+my_list = [] 
+# adding elements to the list
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+# printing the list
+print(my_list)
+
+# inserting elements to the list
+my_list.insert(1, 15)
+# printing the list
+print(my_list)
+
+# extending the list
+my_list.extend([50, 60, 70])
+# printing the list
+print(my_list)
+
+my_list.pop()
+# printing the list 
+print(my_list)
+# sorting the list in ascending oder
+
+my_list.sort()
+# printing the list
+print(my_list)
+# printing the index of 30 in my_list
+print(my_list.index(30))
+
+
